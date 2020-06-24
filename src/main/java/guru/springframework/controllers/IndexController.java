af1f4d91149a67c013141ef208575be0e571dfa2
Octavian Santau <octavian.santau.os@gmail.com>
2020-06-24 22:43:49 +0300

    Add bootstrap data.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,36 +1,27 @@
 package guru.springframework.controllers;
 
-import java.util.Optional;
-
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
-
+/**
+ * Created by jt on 6/1/17.
+ */
 @Controller
 public class IndexController {
-	
-	private final CategoryRepository categoryRepository;
-	private final UnitOfMeasureRepository unitOfMeasureRepository;
-			
-	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {		
-		this.categoryRepository = categoryRepository;
-		this.unitOfMeasureRepository = unitOfMeasureRepository;
-	}
 
+    private final RecipeService recipeService;
 
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
-	@RequestMapping({"","/","/index"})
-	public String getIndexPage() {
-		
-		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-		Optional<UnitOfMeasure> unitOfMeasuerOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-		System.out.println("Cat id is :" + categoryOptional.get().getId());
-		System.out.println("UOM ID is :" + unitOfMeasuerOptional.get().getId());
-		return "index";
-	}
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
 
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,144 +1,148 @@
 package guru.springframework.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
+/**
+ * Created by jt on 6/13/17.
+ */
 @Entity
 public class Recipe {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String description;
-	private Integer prepTime;
-	private Integer cookTime;
-	private Integer servings;
-	private String source;
-	private String url;
-	private String directions;
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-	private Set<Ingredient> ingredients;
-	
-	@Lob
-	private Byte[] image;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@OneToOne(cascade = CascadeType.ALL)
-	private Notes notes;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
 
-	@Enumerated(value = EnumType.STRING)
-	private Difficulty difficulty;
-		
-	@ManyToMany
-	@JoinTable(name="recipe_category", joinColumns = @JoinColumn(name="recipe_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
-	private Set<Category> categories;
-	public Long getId() {
-		return id;
-	}
+    @Lob
+    private String directions;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
 
-	public String getDescription() {
-		return description;
-	}
+    @Lob
+    private Byte[] image;
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
-	public Integer getPrepTime() {
-		return prepTime;
-	}
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
 
-	public void setPrepTime(Integer prepTime) {
-		this.prepTime = prepTime;
-	}
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
-	public Integer getCookTime() {
-		return cookTime;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setCookTime(Integer cookTime) {
-		this.cookTime = cookTime;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Integer getServings() {
-		return servings;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setServings(Integer servings) {
-		this.servings = servings;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getSource() {
-		return source;
-	}
+    public Integer getPrepTime() {
+        return prepTime;
+    }
 
-	public void setSource(String source) {
-		this.source = source;
-	}
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public Integer getCookTime() {
+        return cookTime;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
 
-	public String getDirections() {
-		return directions;
-	}
+    public Integer getServings() {
+        return servings;
+    }
 
-	public void setDirections(String directions) {
-		this.directions = directions;
-	}
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
 
-	public Byte[] getImage() {
-		return image;
-	}
+    public String getSource() {
+        return source;
+    }
 
-	public void setImage(Byte[] image) {
-		this.image = image;
-	}
+    public void setSource(String source) {
+        this.source = source;
+    }
 
-	public Notes getNotes() {
-		return notes;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setNotes(Notes notes) {
-		this.notes = notes;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public Difficulty getDifficulty() {
-		return difficulty;
-	}
-	
-	public void setDifficulty(Difficulty difficulty) {
-		this.difficulty = difficulty;
-	}
-	
-	public Set<Category> getCategories() {
-		return categories;
-	}
-	
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
-	}
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
