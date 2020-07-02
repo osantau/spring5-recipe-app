65900613ff0fe8b9861c46634a983ffa4752ffad
Octavian Santau <octavian.santau@palstor.ro>
2020-07-02 10:29:26 +0300

    Add UnitOfMeasureRepository Test.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -6,6 +6,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import guru.springframework.domain.Recipe;
@@ -36,6 +41,15 @@ class IndexControllerTest {
 		controller = new IndexController(recipeService);
 	}
 
+	@Test
+	public void testMockMvc() throws Exception{
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/")) 
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"));
+	}
+	
+	
 	@Test
 	void testGetIndexPage() {
 		
---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,39 @@
+package guru.springframework.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Before
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindByDescription() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+	void testFindByDescriptionCup() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
